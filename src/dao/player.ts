c555d712a75797b8bbcba1055435385d8025f269
Ilya Cherkasov <ilyakopeysk@gmail.com>
2022-04-09 21:23:34 +0300

    Change a way to mock knex (#3)
    * Change a way to mock knex
    
    * Fix test

--- src/dao/player.ts
+++ src/dao/player.ts
@@ -2,18 +2,24 @@ import db from 'database';
 import playerMapping from 'mapping/player';
 
 class PlayerDao {
+  #tableName
+
+  constructor() {
+    this.#tableName = 'player';
+  }
+
   async getAll(): Promise<Player[]> {
-    return db.from('player').select(playerMapping);
+    return db(this.#tableName).select(playerMapping);
   }
 
   async getByRdgaNumber(rdgaNumber: number): Promise<Player | null> {
-    const player = await db.from('player').select(playerMapping).where({ rdgaNumber });
+    const player = await db(this.#tableName).select(playerMapping).where({ rdga_number: rdgaNumber });
 
     return player?.[0] ?? null;
   }
 
   async createPlayer(player: PlayerDb): Promise<number> {
-    const createdPlayer = await db.insert(player).into('player').returning('*');
+    const createdPlayer = await db(this.#tableName).insert(player).returning('*');
     
     return createdPlayer[0].rdga_number;
   }
