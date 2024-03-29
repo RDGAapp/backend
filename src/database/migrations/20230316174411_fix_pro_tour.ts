65a260ef108834c11fe667b6f3426f92e9ece0f6
Ilya Cherkasov <ilyakopeysk@gmail.com>
2024-03-30 00:21:34 +0300

    remove not generated enums

--- src/database/migrations/20230316174411_fix_pro_tour.ts
+++ src/database/migrations/20230316174411_fix_pro_tour.ts
@@ -1,24 +1,23 @@
-import TournamentType from '../../enums/TournamentType';
 import { Knex } from 'knex';
 
 const tableName = 'tournaments';
 
 export async function up(knex: Knex): Promise<void> {
   await knex(tableName)
-    .where({ tournament_type: TournamentType.Pro })
-    .update({ tournament_type: TournamentType.League });
+    .where({ tournament_type: 'про тур' })
+    .update({ tournament_type: 'лига' });
 
   await knex(tableName)
-    .where({ tournament_type: TournamentType.National })
-    .update({ tournament_type: TournamentType.Pro });
+    .where({ tournament_type: 'национальный тур' })
+    .update({ tournament_type: 'про тур' });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex(tableName)
-    .where({ tournament_type: TournamentType.Pro })
-    .update({ tournament_type: TournamentType.National });
+    .where({ tournament_type: 'про тур' })
+    .update({ tournament_type: 'национальный тур' });
 
   await knex(tableName)
-    .where({ tournament_type: TournamentType.League })
-    .update({ tournament_type: TournamentType.Pro });
+    .where({ tournament_type: 'лига' })
+    .update({ tournament_type: 'про тур' });
 }
